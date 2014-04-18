149eab07269c4de68338be879d06751dbbf0f43d
Ben Mather <bwhmather@bwhmather.com>
2014-04-18 13:32:19 +0100

    a couple of application tests

--- werkzeug_dispatch/testsuite/application.py
+++ werkzeug_dispatch/testsuite/application.py
@@ -10,10 +10,12 @@
 """
 import unittest
 
+from werkzeug.test import Client
 from werkzeug.testsuite import WerkzeugTestCase
 
-from werkzeug import Response
+from werkzeug import Response, BaseResponse
 from werkzeug.routing import Rule
+from werkzeug.exceptions import HTTPException, NotFound
 
 from werkzeug_dispatch.views import expose
 from werkzeug_dispatch.application import Application
@@ -28,6 +30,51 @@ class ApplicationTestCase(WerkzeugTestCase):
         @expose(app.dispatcher, 'index')
         def index(app, request):
             return Response('Hello World')
+
+        client = Client(app, BaseResponse)
+
+        resp = client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.get_data(), b'Hello World')
+
+
+    def test_exception_handlers(self):
+        app = Application()
+
+        @app.exception_handler(BaseException)
+        def default_handler(app, req, exception):
+            return Response('default handler', status=500)
+
+        @app.exception_handler(HTTPException)
+        def werkzeug_handler(app, req, exception):
+            return Response('werkzeug handler', exception.code)
+
+
+        @app.expose(route='/raise_execption')
+        def raise_exception(app, req):
+            raise Exception()
+
+        @app.expose(route='/raise_key_error')
+        def raise_key_error(app, req):
+            raise KeyError()
+
+        @app.expose(route='/raise_404')
+        def raise_404(app, req):
+            raise NotFound()
+
+        client = Client(app, BaseResponse)
+
+        resp = client.get('/raise_execption')
+        self.assertEqual(resp.status_code, 500)
+        self.assertEqual(resp.get_data(), b'default handler')
+
+        resp = client.get('/raise_key_error')
+        self.assertEqual(resp.status_code, 500)
+        self.assertEqual(resp.get_data(), b'default handler')
+
+        resp = client.get('/raise_404')
+        self.assertEqual(resp.status_code, 404)
+        self.assertEqual(resp.get_data(), b'werkzeug handler')
 
 
 def suite():
