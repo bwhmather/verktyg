4a110a48be97cff664818ad05a89c927fea98b28
Ben Mather <bwhmather@bwhmather.com>
2014-05-27 21:38:48 +0100

    don't export quite so much from __init__

--- verktyg/__init__.py
+++ verktyg/__init__.py
@@ -1,17 +1,12 @@
 """
     verktyg
-    ~~~~~~~~~~~~~~~~~
+    ~~~~~~~
 
     :copyright: (c) 2014 by Ben Mather.
     :license: BSD, see LICENSE for more details.
 """
-from verktyg.views import TemplateView, JsonView, ClassView
-from verktyg.views import expose, expose_html, expose_json
+from verktyg.routing import URLMap
 from verktyg.dispatch import Dispatcher
 from verktyg.application import Application
 
-__all__ = [
-    'TemplateView', 'JsonView', 'ClassView',
-    'expose', 'expose_html', 'expose_json',
-    'Dispatcher', 'Application',
-    ]
+__all__ = ['URLMap', 'Dispatcher', 'Application']
