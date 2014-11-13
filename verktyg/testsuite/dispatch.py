58b1e8b195744a3bab93b9c9c265b23676ab0cfd
Ben Mather <bwhmather@bwhmather.com>
2014-11-13 12:57:33 +0000

    dispatcher returns bindings, not binding actions

--- verktyg/testsuite/dispatch.py
+++ verktyg/testsuite/dispatch.py
@@ -17,37 +17,49 @@ from werkzeug.exceptions import NotImplemented, MethodNotAllowed, NotAcceptable
 from verktyg.dispatch import Binding, Dispatcher
 
 
+def _make_view(value):
+    def view(app, req):
+        return value
+    return view
+
+
 class DispatchTestCase(WerkzeugTestCase):
     def test_name_dispatch(self):
         dispatcher = Dispatcher([
-            Binding('tweedle-dum', 'Tweedle Dum'),
-            Binding('tweedle-dee', 'Tweedle Dee'),
-            Binding('same', 'overridden'),
-            Binding('same', 'overriding'),
+            Binding('tweedle-dum', _make_view('Tweedle Dum')),
+            Binding('tweedle-dee', _make_view('Tweedle Dee')),
+            Binding('same', _make_view('overridden')),
+            Binding('same', _make_view('overriding')),
             ])
 
-        self.assert_equal('Tweedle Dum', dispatcher.lookup('tweedle-dum'))
-        self.assert_equal('Tweedle Dee', dispatcher.lookup('tweedle-dee'))
+        self.assert_equal(
+            'Tweedle Dum', dispatcher.lookup('tweedle-dum')(None, None)
+        )
+        self.assert_equal(
+            'Tweedle Dee', dispatcher.lookup('tweedle-dee')(None, None)
+        )
         self.assert_raises(NotImplemented, dispatcher.lookup, 'non-existant')
-        self.assert_equal('overriding', dispatcher.lookup('same'))
+        self.assert_equal(
+            'overriding', dispatcher.lookup('same')(None, None)
+        )
 
     def test_method_dispatch(self):
         dispatcher = Dispatcher([
-            Binding('test', 'get', method='GET'),
-            Binding('test', 'post', method='POST'),
-            Binding('head', 'head', method='HEAD'),
-            Binding('no-head', 'get', method='GET'),
+            Binding('test', _make_view('get'), method='GET'),
+            Binding('test', _make_view('post'), method='POST'),
+            Binding('head', _make_view('head'), method='HEAD'),
+            Binding('no-head', _make_view('get'), method='GET'),
             ])
 
         # default to 'GET'
         self.assert_equal('get',
-                          dispatcher.lookup('test'))
+                          dispatcher.lookup('test')(None, None))
         self.assert_equal('get',
-                          dispatcher.lookup('test', method='GET'))
+                          dispatcher.lookup('test', method='GET')(None, None))
 
         # `POST` gives something different
         self.assert_equal('post',
-                          dispatcher.lookup('test', method='POST'))
+                          dispatcher.lookup('test', method='POST')(None, None))
 
         # `PUT` not found
         self.assert_raises(MethodNotAllowed,
@@ -55,61 +67,75 @@ class DispatchTestCase(WerkzeugTestCase):
 
     def test_head_fallback(self):
         dispatcher = Dispatcher([
-            Binding('head', 'head', method='HEAD'),
-            Binding('no-head', 'get', method='GET'),
+            Binding('head', _make_view('head'), method='HEAD'),
+            Binding('no-head', _make_view('get'), method='GET'),
             ])
 
         # `HEAD` should fall back to `GET`
-        self.assert_equal('head',
-                          dispatcher.lookup('head', method='HEAD'))
-        self.assert_equal('get',
-                          dispatcher.lookup('no-head', method='HEAD'))
+        self.assert_equal(
+            'head', dispatcher.lookup('head', method='HEAD')(None, None)
+        )
+        self.assert_equal(
+            'get', dispatcher.lookup('no-head', method='HEAD')(None, None)
+        )
 
     def test_method_override(self):
         dispatcher = Dispatcher([
-            Binding('same', 'overridden'),
-            Binding('same', 'unaffected', method='POST'),
-            Binding('same', 'overriding'),
+            Binding('same', _make_view('overridden')),
+            Binding('same', _make_view('unaffected'), method='POST'),
+            Binding('same', _make_view('overriding')),
             ])
 
         # replacing handler for one method should not affect others
         self.assert_equal('overriding',
-                          dispatcher.lookup('same'))
+                          dispatcher.lookup('same')(None, None))
         self.assert_equal('unaffected',
-                          dispatcher.lookup('same', method='POST'))
+                          dispatcher.lookup('same', method='POST')(None, None))
 
     def test_accept_dispatch(self):
         dispatcher = Dispatcher([
-            Binding('test', 'json', content_type='application/json'),
-            Binding('test', 'html', content_type='text/html'),
-            Binding('test', 'whatever'),
-            Binding('nope', 'nope', content_type='application/xml'),
-            ])
+            Binding(
+                'test', _make_view('json'),
+                content_type='application/json'
+            ),
+            Binding(
+                'test', _make_view('html'),
+                content_type='text/html'
+            ),
+            Binding(
+                'test', _make_view('whatever')
+            ),
+            Binding(
+                'nope', _make_view('nope'),
+                content_type='application/xml'
+            ),
+        ])
 
         # accept header strings
         self.assert_equal(
             'json',
-            dispatcher.lookup('test', accept='application/json')
+            dispatcher.lookup('test', accept='application/json')(None, None)
         )
 
         self.assert_equal(
             'json',
             dispatcher.lookup(
                 'test', accept='application/json; q=0.9, text/html; q=0.8'
-            )
+            )(None, None)
         )
         self.assert_equal(
             'whatever',
-            dispatcher.lookup('test', accept='application/xml')
+            dispatcher.lookup('test', accept='application/xml')(None, None)
         )
 
         self.assert_raises(
             NotAcceptable,
-            dispatcher.lookup, 'nope', accept='text/html')
+            dispatcher.lookup, 'nope', accept='text/html'
+        )
 
     def test_nested(self):
         child = Dispatcher([
-            Binding('nested', 'Nested'),
+            Binding('nested', _make_view('Nested')),
             ])
         parent = Dispatcher([
             child,
@@ -117,7 +143,7 @@ class DispatchTestCase(WerkzeugTestCase):
 
         self.assert_equal(
             'Nested',
-            parent.lookup('nested'))
+            parent.lookup('nested')(None, None))
 
 
 def suite():
