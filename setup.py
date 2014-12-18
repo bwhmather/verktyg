a2904eb34787440a095e8b5b66b8af00f4ef8765
Ben Mather <bwhmather@bwhmather.com>
2014-12-18 12:12:01 +0000

    run tests using python setup.py test

--- setup.py
+++ setup.py
@@ -34,4 +34,5 @@ setup(
         ],
     packages=find_packages(),
     include_package_data=True,
+    test_suite='verktyg.testsuite.suite',
 )
--- verktyg/testsuite/__init__.py
+++ verktyg/testsuite/__init__.py
@@ -10,26 +10,6 @@ import unittest
 import werkzeug.testsuite as wzt
 
 
-class BetterLoader(wzt.BetterLoader):
-    """Werkzeug loader saves werkzeug root suite in closure.  This just
-    overrides it.
-    """
-    def getRootSuite(self):
-        return suite()
-
-
-def suite():
-    suite = unittest.TestSuite()
-    for other_suite in wzt.iter_suites(__name__):
-        suite.addTest(other_suite)
-    return suite
-
-
-def main():
-    try:
-        unittest.main(testLoader=BetterLoader(), defaultTest='suite')
-    except Exception:
-        import sys
-        import traceback
-        traceback.print_exc()
-        sys.exit(1)
+suite = unittest.TestSuite()
+for other_suite in wzt.iter_suites(__name__):
+    suite.addTest(other_suite)
