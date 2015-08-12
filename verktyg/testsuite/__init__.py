f36014b45dabbd0eb0c6412cb6a51562c76f4f2d
Ben Mather <bwhmather@bwhmather.com>
2015-08-12 16:54:28 +0100

    run tests in dependency order

--- verktyg/testsuite/__init__.py
+++ verktyg/testsuite/__init__.py
@@ -11,23 +11,23 @@
 import unittest
 
 from verktyg.testsuite import (
-    test_datastructures, test_accept, test_application, test_dispatch,
-    test_exceptions, test_routing, test_views, test_requests, test_responses,
-    test_wsgi, test_http,
+    test_datastructures, test_exceptions, test_http, test_wsgi,
+    test_requests, test_responses, test_routing, test_dispatch,
+    test_accept, test_views, test_application,
 )
 
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite((
     loader.loadTestsFromModule(test_datastructures),
-    loader.loadTestsFromModule(test_accept),
-    loader.loadTestsFromModule(test_application),
-    loader.loadTestsFromModule(test_dispatch),
     loader.loadTestsFromModule(test_exceptions),
-    loader.loadTestsFromModule(test_routing),
-    loader.loadTestsFromModule(test_views),
+    loader.loadTestsFromModule(test_http),
+    loader.loadTestsFromModule(test_wsgi),
     loader.loadTestsFromModule(test_requests),
     loader.loadTestsFromModule(test_responses),
-    loader.loadTestsFromModule(test_wsgi),
-    loader.loadTestsFromModule(test_http),
+    loader.loadTestsFromModule(test_routing),
+    loader.loadTestsFromModule(test_dispatch),
+    loader.loadTestsFromModule(test_accept),
+    loader.loadTestsFromModule(test_views),
+    loader.loadTestsFromModule(test_application),
 ))
