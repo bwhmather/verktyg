b8b97854e7bf79f7e5111edaf6ca7252327e024d
Ben Mather <bwhmather@bwhmather.com>
2015-09-09 22:06:20 +0100

    bump version to 0.8.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extras_require = {
 
 setup(
     name='verktyg',
-    version='0.8.3',
+    version='0.8.4',
     url='https://github.com/bwhmather/verktyg',
     license='BSD',
     author='Ben Mather',
